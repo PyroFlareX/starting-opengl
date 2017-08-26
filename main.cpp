ab4efc4e6c454c78b6fc75624167680b545da99a
PyroFlareX <31327816+PyroFlareX@users.noreply.github.com>
2017-08-26 13:32:32 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "display.h"
+#include <GL/glew.h>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello Idiots!" << std::endl;
+    Display display(800, 600, "Hello World!");
+    while(!display.IsClosed())
+    {
+        glClearColor(0.0f, 0.15f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        display.Update();
+    }
+    return 0;
+}
+
